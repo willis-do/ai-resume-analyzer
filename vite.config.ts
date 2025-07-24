34fcf77d2e983fdd80526bfd9a81db26e72b3ceb
Willis Do <willisdo2001@gmai.com>
2025-07-24 02:21:03 -0400

    update homepage, navbar, and resume card

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import react from "@vitejs/plugin-react";
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
-
